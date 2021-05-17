194034de85ad86bdb5bc7d9b3778e9322f00390e
Edgar <edgar_demin@mail.ru>
2021-05-17 22:27:36 +0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,24 @@ $(document).ready(function(){
     focusOnSelect: true, 
   });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header__menu'),
+  menuItem = document.querySelectorAll('.header__item'),
+  hamburger = document.querySelector('.header__hamburger'),
+  connect = document.querySelector('.header__connect');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('header__hamburger_active');
+      menu.classList.toggle('header__menu_active');
+      connect.classList.toggle('header__connect_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('top-menu_active');
+          connect.classList.toggle('header__connect_active');
+      })
+  })
+});
